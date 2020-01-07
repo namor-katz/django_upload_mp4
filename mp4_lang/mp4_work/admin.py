36d768d76036a954d0e06d51888213ef2ffb9a5b
Roman <namor925@gmail.com>
2020-01-07 22:35:52 +0700

    check size file, check endswitch file, write to database, create tag "convert"

--- mp4_lang/mp4_work/admin.py
+++ mp4_lang/mp4_work/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Mp4
+from django.contrib.auth.models import User, Group
 # Register your models here.
+#unregister standard models
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
-#admin.site.register(Mp4)
+#admin.site.register(Mp4)  #old
 @admin.register(Mp4)
 class pMp4(admin.ModelAdmin):
-    list_display = ("file_path",  "desc") #это что. переопределение __string-a?
+    list_display = ("file_path",  "desc", "file_convert",  "uploaded_at") #это что. переопределение __string-a?
     prepopulated_fields = {'file_name': ("file_path", )}
+    ordering = ('uploaded_at', )
--- mp4_lang/mp4_work/models.py
+++ mp4_lang/mp4_work/models.py
@@ -1,19 +1,54 @@
 from django.db import models
 #import uuid
+from django.core.exceptions import ValidationError
+import ffmpy
+
+convert = None
+
+def validate_video(image):
+    """ validate video 1) extends 2) size """
+    file_size = image.file.size
+    if image.name.endswith("mp4"):
+        pass
+    else:
+        raise ValidationError("This not mp4!")
+
+    limit_kb = 2500
+    global convert
+    if file_size > limit_kb * 1024:
+        convert = True
+        #raise ValidationError("Max size of file is %s KB" % limit_kb)
+    else:
+        #pass
+        convert = False
+
+    #limit_mb = 8
+    #if file_size > limit_mb * 1024 * 1024:
+    #    raise ValidationError("Max size of file is %s MB" % limit_mb)
+
 
 class Mp4(models.Model):
+    #image = models.ImageField('Image', upload_to=image_upload_path, validators=[validate_image])
     file_name = models.CharField(max_length=100,  blank=True,  null=True) #или уид. надо еще прикинуть.
-    file_path = models.FileField(max_length=500,  verbose_name='загрузить файл!',  upload_to='archives/%Y/%m/%d/')
+    file_path = models.FileField(max_length=500,  verbose_name='загрузить файл!',  upload_to='archives/%Y/%m/%d/',  validators=[validate_video])
     file_type = models.CharField(max_length=100,  blank=True,  null=True)
     file_size = models.IntegerField(blank=True,  null=True)
     file_hash = models.CharField(max_length=100,  blank=True,  null=True)
     desc = models.CharField(max_length=100, verbose_name='описание') #опционально - описание
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    file_convert = models.BooleanField(default=False)
     
     class Meta:
         verbose_name = "Видеоролик"
         verbose_name_plural = "Видеоролики"
-        
+
     def __str__(self):
         return self.desc
         
+    def save(self,  *args,  **kwargs):
+        global convert
+        if convert is True:
+            self.file_convert = True
+        else:
+            self.file_convert = False
+        super(Mp4, self).save(*args, **kwargs)
